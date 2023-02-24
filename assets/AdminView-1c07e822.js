93af34f7a0106acf894b7099a65033e761b3f19a
susu3131 <anny81327@gmail.com>
2023-02-24 16:26:45 +0800

    deploy

---
+++ assets/AdminView-1c07e822.js
@@ -0,0 +1,1 @@
+import{o as e,c as t,a as o}from"./index-7194e4d9.js";const a=o("h1",null,"後台訂單管理",-1),n=[a],l={__name:"AdminView",setup(s){return(c,_)=>(e(),t("main",null,n))}};export{l as default};
---
+++ assets/HomeView-a395ff13.js
@@ -0,0 +1,1 @@
+import{o as e,c as o,a as t}from"./index-7194e4d9.js";const a=t("h1",null,"首頁",-1),s=[a],l={__name:"HomeView",setup(c){return(n,_)=>(e(),o("main",null,s))}};export{l as default};
